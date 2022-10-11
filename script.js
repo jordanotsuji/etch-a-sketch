9c4f21c83e5692bae84f5caee211234889a5df0d
jordanotsuji <18375877+jordanotsuji@users.noreply.github.com>
2022-10-10 16:29:51 -1000

    added button single toggle style

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let gridSize = 16;
 let mouseDown = false;
+let drawMode = "default";   // drawMode = button id of current drawing mode
+
 const sketchContainer = document.getElementById('sketch-container');
-const clearButton = document.querySelector('.clear-button')
+const colorButtons = document.querySelectorAll('.toggleable');
+const clearButton = document.getElementById('clear-button');
+const rainbowButton = document.getElementById('rainbow-button');
+const eraserButton = document.getElementById('eraser-button');
 
 function initGrid() {
   sketchContainer.innerHTML = ""
@@ -18,24 +23,43 @@ function initGrid() {
 
 function colorTile(e) {
   // only color tiles if hovering over a tile and holding click
-  if (e.type === "mouseover" && mouseDown) {
-    e.target.style.backgroundColor = "black";
-  }
+  if (e.type === "mouseover" && !mouseDown) return;
+  e.target.style.backgroundColor = "black";
 }
 
 function clearGrid() {
   // resets background color of each tile in sketch zone,
   // not sure if this is more efficient than recalling initGrid
   const tiles = document.querySelectorAll('.tile');
-  tiles.forEach(tile => tile.style.backgroundColor = "")
+  tiles.forEach(tile => tile.style.backgroundColor = "");
+}
+
+function eraserButtonHandler(e) {
+  if (drawMode !== e.target.id) {
+    drawMode = e.target.id;
+    toggleButton(e.target);
+  } else {
+    drawMode = "default";
+    toggleButton();
+  }
+}
+
+function toggleButton(target) {
+  colorButtons.forEach(button => button.classList.remove('toggled'))
+  console.log(target)
+  if (target != null) {
+    target.classList.add('toggled')
+  }
 }
 
 function initEventListeners() {
   document.body.onmousedown = () => mouseDown = true;
   document.body.onmouseup = () => mouseDown = false;
-  clearButton.addEventListener('click', clearGrid)
+  clearButton.addEventListener('click', clearGrid);
+  eraserButton.addEventListener('click', eraserButtonHandler);
+  rainbowButton.addEventListener('click', eraserButtonHandler);
 }
 
-initGrid()
-initEventListeners()
+initGrid();
+initEventListeners();
 
